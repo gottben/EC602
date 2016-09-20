3d5d1cad2e2a190a32c4b5da43c96f884f3581fc
Brian Appleton <appleton@bu.edu>
2016-09-20 12:41:40 -0400

    part b is now working

--- HW3/w3b_main.cpp
+++ HW3/w3b_main.cpp
@@ -3,22 +3,25 @@ using namespace std;
 
 #include "w3b_bigint.cpp"
 
-int main() {
-	
-	string b = "111111";
-	string c = "1111111";
-	string d = multiply_int(b,c);
-	cout << d << endl;
-	
-	/*	
-	//testing	
-	string e = to_string(52);
-	cout << e << endl;
-	int p = atoi(b.c_str());
-	cout << p << endl;
-	*/
+void print_test_case(string a, string b);
 
+int main() {
+	string a = "3450823583275802374528348572384562356";
+	string b = "6032";
+	string c = "596"; 
+	string d = "111111";
+	string e = "1111111";
+	string f = "999999";
+	
+	print_test_case(d,e);
+	print_test_case(f,b);
+	print_test_case(a,f);
+	
 	return 0;
 	
 
+}
+
+void print_test_case(string a, string b) {
+	cout << a << " * " << b << " = " << multiply_int(a,b) << endl;
 }
