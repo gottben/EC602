39d8b2f1b3985a95b4945872bf5f037d82a249c6
gottbenn <gottbenn@bu.edu>
2016-09-20 09:12:30 -0400

    Final versions of PartA

---
+++ w3a_main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+#include "w3a_polyops.cpp"
+
+void print_poly(Poly &a, char func);
+double evaluate_poly(Poly &a, int x);
+
+int main() {
+	//test program
+	
+	//polynomials used for testing
+	//zero-based index is the exponent for the term; the contents of the array at the respective index is the coefficient
+	Poly a = {1,2,3};
+	Poly b = {4,5,6,1,0};
+
+	//print polynomials	
+	print_poly(a,'a');
+	cout << "a(10) = " << evaluate_poly(a,10) << endl;
+	print_poly(b,'b');
+	cout << "b(10) = " << evaluate_poly(b,10) << endl;
+
+	//test addition
+	cout << "c(x) = a(x) + b(x): " << endl;
+	Poly c = add_poly(a,b);
+	print_poly(c,'c');
+	cout << "c(10) = " << evaluate_poly(c,10) << endl;
+
+	//test multiplication
+	cout << "d(x) = a(x) * b(x): " << endl;
+	Poly d = multiply_poly(a,b);
+	print_poly(d,'d');
+	cout << "d(10) = " << evaluate_poly(d,10) << endl;
+
+	return 0;
+}
+
+void print_poly(Poly &a, char func) {
+	cout << func <<"(x) = ";
+	for (int i=a.size()-1; i>0; i--) {
+		cout << a[i] << "x^" << i << " + ";	
+	}
+	cout << a[0] << "x^" << "0" << endl;
+}
+
+double evaluate_poly(Poly &a, int x) {
+	double result = 0;	
+	for (int i = 0; i<a.size(); i++) {
+		result += a[i]*pow(x,i);
+	}
+	return result;
+}
--- w3a_polyops.cpp
+++ w3a_polyops.cpp
@@ -13,8 +13,8 @@ Poly multiply_poly(const Poly &a, const Poly &b);
 
 Poly add_poly(const Poly &a, const Poly &b)
 {
-	size_a = a.size();
-	size_b = b.size();
+	int size_a = a.size();
+	int size_b = b.size();
 	
 	Poly c;
 	// check which polynomial is greater
@@ -27,13 +27,13 @@ Poly add_poly(const Poly &a, const Poly &b)
 
 		// sum up all values between 0 and 
 		// the size of b
-		for(int i = 0; i++; (size_b-1))
+		for(int i = 0; i < size_b; i++)
 		{
 			c[i] = a[i] + b[i];
 		}
 		// add all values between the end of b
 		// to the end of a to C
-		for(int j = size_b; j++; (size_a -1))
+		for(int j = size_b; j < size_a; j++)
 		{
 			c[j] = a[j];
 		}
@@ -48,13 +48,13 @@ Poly add_poly(const Poly &a, const Poly &b)
 
 		// sum up all values between 0 and
 		// the size of a
-		for(int i = 0; i++; (size_a-1))
+		for(int i = 0; i <size_a; i++)
 		{
 			c[i] = a[i] + b[i];
 		}
 		// add all values between the end of a
 		// to the end of b into C
-		for(int j = size_a; j++; (size_b -1))
+		for(int j = size_a; j<size_b; j++)
 		{
 			c[j] = b[j];
 		}
@@ -65,8 +65,8 @@ Poly add_poly(const Poly &a, const Poly &b)
 
 Poly multiply_poly(const Poly &a, const Poly &b)
 {
-	size_a = a.size();
-	size_b = b.size();
+	int size_a = a.size();
+	int size_b = b.size();
 	
 	Poly c;
 	// check which polynomial is greater
@@ -74,23 +74,28 @@ Poly multiply_poly(const Poly &a, const Poly &b)
 	// create the polynomial that shall be returned. 
 	// the maximum length of any two polynomials multipled together is 
 	// the product of the sizes of the two polynomials being multiplied
-	c.assign(size_b * size_a,0); 
+	c.assign((size_b + size_a)-1,0); 
 
 	// keeps track of how many times we have performed the multiplication
 	int keep_track = 0; 
 
 	// multiply vales in this manner a[1]*b[1] + a[1]*b[2] etc. etc. 
-	for(int i = 0; i++; (size_a-1))
+	for(int i = 0; i < size_a ; i++)
 	{
-		for(int j = 0; j++; (size_b - 1))
+		for(int j = 0; j < size_b; j++)
 		{
-			for(int k = 0; k++; (size_a - 1))
-			{
-				c[k+(size_a * keep_track)] = a[i] + b[j]; 
-			}
-
-			keep_track++; 
+			c[j + keep_track] += a[i]*b[j];
+			cout << j+ keep_track << endl;
+			// Wasn't sure what I was thinking here...
+			// Wrote this for loop under some poor assumptions
+			//for(int k = 0; k++; (size_a - 1))
+			//{
+			//	c[k+(size_a * keep_track)] = a[i] + b[j]; 
+			//	j++; 
+			//}
+			//keep_track++; 
 		} 
+		keep_track++; 
 	}
 		return c; 
 }
