1eb405e79fbc6b8cb7becced709077ff6e53fb6d
appletonbrian <appleton@bu.edu>
2016-09-28 21:17:09 -0400

    Originating draft solutions and test script

---
+++ HW4/w4_polynomial.py
@@ -0,0 +1,189 @@
+# AUTHOR BrianAppleton appleton@bu.edu
+
+'''
+PYTHON POLYNOMIAL CLASS
+
+---- Functionality checklist ----
+[x] initialize with constructor, efficient storage
+[x] evaluate polynomials using syntax like p.eval(10) where 10=x
+[x] implement str() and repr() **Not required but will be used by checker**
+[x] access and modify the coefficients using []. p[8] should set coeff of x^8 to 12. Make sure coefficients of 0 aren't stored.
+[x] add polynomials using +, forward implementation only. Make sure that the expression x+y doesn't change the values of x or y.
+[x] subtract polynomials using -, forward implementation only. Make sure that the expression x-y doesn't change the values of x or y.
+[x] multiply polynomials using *. Make sure that the expression x*y doesn't change the values of x or y.
+[x] test for equality using ==
+[x] implement p.deriv() method
+[ ] ensure negative powers work
+[ ] ensure int, float, and complex coefficients work
+[ ] ensure int, float, and complex x works
+[x] make sure that polynomials are stored efficiently
+
+
+---- Helpful references ----
+Python dictionaries:  https://docs.python.org/3/tutorial/datastructures.html#dictionaries
+Python magic methods: http://www.rafekettler.com/magicmethods.html
+
+'''
+
+class Polynomial():
+	def __init__(self, p_input=[]):
+		"""		
+		The last element in p_input list is the coefficient of x^0
+		Create a dictionary p_dict to represent the input polynomial. It will use the exponents as the keys.
+		Efficient storage is partially implemented here. Coefficients of 0 will not be stored.	
+		"""		
+		self.p_dict = {}
+		exp = len(p_input)-1
+		for coeff in p_input:
+			if coeff!=0:			
+				self.p_dict[exp] = coeff
+			exp=exp-1
+	def eval(self,x):
+		result = 0 #defaults to an int container	
+		for exp, coeff in self.p_dict.items():
+			result += coeff*x**exp
+		return result
+		
+	
+	def __str__(self):
+		"""
+		Return nicely-formatted string showing polynomial
+		This seems to matter less
+		"""		
+		result = ""		
+		for exp, coeff in self.p_dict.items():
+			result += "(" + str(coeff) + ")" + "x^" + str(exp) + " + "
+		return result[0:len(result)-2]	
+	
+	def __repr__(self):
+		"""
+		Return dictionary representation of polynomial
+		"""
+		return(str(self.p_dict))
+	
+			
+	def __setitem__(self, exp, coeff):
+		"""
+		Required by mutable container protocol
+		Usage: p[x] = y
+		This is functional for pre-existing or non-existent exp		
+		"""		
+		if coeff != 0:		
+			self.p_dict[exp] = coeff
+	def __len__(self):
+		"""
+		Required by mutable container protocol
+		Usage: len(p)
+		"""
+		return len(self.p_dict)
+	def __getitem__(self, exp):
+		"""
+		Required by mutable container protocol
+		Usage: p[x]
+		Return 0 for missing exponents
+		"""		
+		if exp in self.p_dict:
+			return self.p_dict[exp]
+		else:
+			return 0
+	def __delitem__(self, exp):
+		"""
+		Required by mutable container protocol
+		"""
+		self.p_dict.pop(exp)
+	def __add__(self,p_add):
+		"""
+		Arguments are polynomials
+		Argument.p_dict gets the dictionary
+		"""		
+		#p_result = self
+		#Create a copy of self
+		p_result = Polynomial()
+		for exp, coeff in self.p_dict.items():
+			p_result[exp] = coeff
+		
+		#Loop through p_add and modify terms of p_result
+		for exp, coeff in p_add.p_dict.items():		
+			if exp in p_result.p_dict:
+				p_result.p_dict[exp] += coeff
+			else:
+				p_result.p_dict[exp] = coeff	
+		return p_result
+	def __radd__(self,p):
+		"""
+		Is this needed?
+		"""		
+		pass
+	def __sub__(self,p_sub):
+		"""
+		Subtract p_sub from self and return result
+		Usage: -
+		"""
+		#p_result = self;
+		#Create a copy of self
+		p_result = Polynomial()
+		for exp, coeff in self.p_dict.items():
+			p_result[exp] = coeff
+		
+		#Loop through p_sub and modify terms of p_result
+		for exp, coeff in p_sub.p_dict.items():		
+			if exp in p_result.p_dict:
+				p_result.p_dict[exp] -= coeff
+			else:
+				p_result.p_dict[exp] = -1*coeff	
+		return p_result
+	def __mul__(self, p_mul):
+		"""
+		Multiply two polynomials and return a Polynomial
+		Usage: *
+		"""		
+		
+		p_result = Polynomial()
+		
+		for exp_a, coeff_a in self.p_dict.items():
+			for exp_b, coeff_b in p_mul.p_dict.items():
+				if (exp_a+exp_b) in p_result.p_dict:					
+					p_result.p_dict[exp_a+exp_b] += coeff_a*coeff_b	
+					#print("Case 1: ", exp_a+exp_b, p_result.p_dict[exp_a+exp_b])		
+				else:
+					p_result.p_dict[exp_a+exp_b] =  coeff_a*coeff_b
+					#print("Case 2: ", exp_a+exp_b, p_result.p_dict[exp_a+exp_b])
+		
+		return p_result
+	def __eq__(self, p_eq):
+		"""
+		Test for equality
+		The only way that two polynomials are surely equal is if all of their coefficients and exponents match
+		Given a base, there could be instances where the coefficients/exponents are not equal, but the polynomials evaluate to the same value
+		Usage: ==
+		"""
+		return self.p_dict==p_eq.p_dict
+	def deriv(self):
+		"""
+		Take derivative of the polynomial with respect to its base x
+		"""
+		result = Polynomial()
+		for exp, coeff in self.p_dict.items():
+			result[exp-1]=coeff*exp
+		return result
+
+def main():
+	"""	
+	x = Polynomial([0,0,5j,2,1])
+	y = Polynomial([0,0,0,4,3])
+	z = Polynomial()
+	z[1] = 4
+	z[10] = 8 
+
+	print(str(y))
+	print(str(y.deriv()))
+	print(str(z))
+	print(str(z.deriv()))
+
+	print(repr(z))
+
+	"""
+    
+
+if __name__=="__main__":
+    main()
---
+++ HW4/w4_tester.py
@@ -0,0 +1,136 @@
+from w4_polynomial import Polynomial
+
+'''
+---- Functionality checklist ----
+[x] initialize with constructor, efficient storage
+[x] evaluate polynomials using syntax like p.eval(10) where 10=x
+[x] implement str() and repr() **Not required but will be used by checker**
+[x] access and modify the coefficients using []. p[8] should set coeff of x^8 to 12. Make sure coefficients of 0 aren't stored.
+[x] add polynomials using +, forward implementation only. Make sure that the expression x+y doesn't change the values of x or y.
+[x] subtract polynomials using -, forward implementation only. Make sure that the expression x-y doesn't change the values of x or y.
+[x] multiply polynomials using *. Make sure that the expression x*y doesn't change the values of x or y.
+[x] test for equality using ==
+[x] implement p.deriv() method
+[ ] ensure negative powers work
+[ ] ensure int, float, and complex coefficients work
+[ ] ensure int, float, and complex x works
+[x] make sure that polynomials are stored efficiently
+'''
+
+'''
+NOTE: This testing code requires that __repr__ is defined as follows in the Polynomial class:
+	def __repr__(self):
+		"""
+		Return dictionary representation of polynomial
+		"""
+		return(str(self.p_dict))
+'''
+
+
+'''
+Set up test Polynomials
+Dictionary contents retreived with repr():
+x={0: 4, -4: (1+1j), -5: 2, 4: 3}
+y={0: (10-3j), 1: 2.7, 2: -1, 5: 17.9}
+'''
+x = Polynomial([3,0,0,0,4])
+y = Polynomial([-1, 2.7, -3j+10])
+z = Polynomial([1,2,5,6,7,0,1])
+w = Polynomial([0,0,3,2,0])
+
+x[-5] = 2
+x[-4] = 1j+1
+y[5]  = 17.9
+
+'''
+Test str() and repr()
+'''
+if 'object' in repr(x):
+	print("WARNING: repr() function is not defined for Polynomial")
+if 'object' in str(x):
+	print("WARNING: str() function is not defined for Polynomial")
+'''
+
+Test initialization,  ==, and assignment
+'''
+if y == Polynomial([17.9, 0, 0, -1, 2.7, 10-3j]):
+	print("PASS: __init__, __eq__, and __setitem__ are functional")
+else:
+	print("FAIL: ", repr(Polynomial([17.9, 0, 0, -1, 2.7, 10-3j])), " does not match y = ", repr(y))
+
+
+'''
+Make sure all Polynomial methods except __setitem__ do not modify input args. Use z as test case
+
+'''
+
+junk = z-w
+junk = w-z
+junk = z*w
+junk = w*z
+junk = z.eval(3)
+junk = z-w
+junk = w-z
+junk = z.deriv()
+junk = z[1500]
+junk = z[2] + z[1]
+junk = z[1]==0
+
+if z == Polynomial([1,2,5,6,7,0,1]):
+	print("PASS: no methods modify their arguments")
+else:
+	print("FAIL: one or more methods modify their input arguments")
+
+'''
+Test eval
+'''
+if y.eval(3j) == (19+4354.8j) and x.eval(-3) == (247.00411522633746+0.012345679012345678j) and z.eval(0) == 1:
+	print("PASS: eval() is functional")
+else:
+	print("PASS: eval() not functional")
+
+'''
+Test __getitem__
+'''
+if (x[-5] == 2 and y[0] == (10-3j) and x[50000] == 0):
+	print("PASS: __getitem__ is functional")
+else:
+	print("FAIL: __getitem__ is not functional")
+
+'''
+Test __add__
+'''
+
+if ((z+w).eval(10)) == (z.eval(10) + w.eval(10)):
+	print("PASS: __add__ is functional")
+else:
+	print("FAIL: __add__ is not functional, expected ", z.eval(10) + w.eval(10), " to equal " , (z+w).eval(10))
+
+'''
+Test __sub__
+'''
+if ((z-w).eval(10)) == (z.eval(10) - w.eval(10)):
+	print("PASS: __sub__ is functional")
+else:
+	print("FAIL: __sub__ is not functional, expected ", z.eval(10) - w.eval(10), " to equal " , (z-w).eval(10))
+
+'''
+Test __mul__
+'''
+if ((z*w).eval(10)) == (z.eval(10) * w.eval(10)):
+	print("PASS: __mul__ is functional")
+else:
+	print("FAIL: __mul__ is not functional, expected ", z.eval(10) * w.eval(10), " to equal " , (z*w).eval(10))
+
+'''
+Test deriv()
+'''
+if repr(x.deriv()) == '{3: 12, -6: -10, -5: (-4-4j)}':
+	print("PASS: deriv() is functional")
+else:
+	print("FAIL: deriv() is not functional or repr() is not defined as needed for this script.")
+
+
+
+
+
