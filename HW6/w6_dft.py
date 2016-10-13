eaaa05d80023e072a6bbd1704b690bf7ea5b17ad
Brian Appleton <appleton@bu.edu>
2016-10-13 10:54:25 -0400

    my DFT is DTF

---
+++ HW6/w6_dft.py
@@ -0,0 +1,25 @@
+from numpy import zeros,exp,array,pi
+
+
+def DFT(x):	
+	
+	try:
+		N = len(x)	
+		X = zeros(N, dtype="complex")		
+		#compute the DFT of x and store it in X
+		for k in range(0,N):
+			#for each element in X, perform the DFT summation
+			for n,x_n in enumerate(x):				
+				X[k]+=x_n*exp(-2j*pi*n*k/N)
+		return X
+	except:
+		raise ValueError("You broke me!")
+
+def main():
+	x = array([1,2,3])
+	X = DFT(x)
+	print("x: ", x)
+	print("X1: ", X)
+
+if __name__=="__main__":
+	main()
