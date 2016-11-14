c94bae365a6b4bc32e94a3c445fd97d8020cd529
gottbenn <gottbenn@bu.edu>
2016-11-14 11:48:07 -0500

    faster version of partA

--- HW9and10/w9_wordplayer2.py
+++ HW9and10/w9_wordplayer2.py
@@ -30,7 +30,7 @@ def main(argv):
             N = int(N) + int(1)
 
             for key, value in the_dict.items():
-                if key in l_list and N in the_dict[key]:
+                if key in l_list:
                     for word in the_dict[key][N]:
                         n_list = l_list
                         for c in word:
@@ -56,3 +56,55 @@ def main(argv):
 
 if __name__ == "__main__":
     main(sys.argv[1:])
+
+
+# #!/usr/bin/python3.5
+# # AUTHOR AlexBennett gottbenn@bu.edu
+# import sys
+
+
+# def main(argv):
+#     d_words = {}
+#     with open(argv[0]) as f:
+#         for line in f:
+#             N = len(line)
+#             if N not in d_words:
+#                 d_words[N] = [line[0:N - 1]]
+#             else:
+#                 d_words[N].append(line[0:N - 1])
+
+#     letter_list, N = input().split()
+#     for i in range(0, 10000000):
+#         try:
+#             result = []
+
+#             if N == "0":
+#                 break
+
+#             N = int(N) + int(1)
+
+#             for word in d_words[N]:
+#                 new_list = letter_list
+#                 for c in word:
+#                     if c in new_list:
+#                         new_list = new_list.replace(c, "", 1)
+#                         if (len(letter_list) - len(word)) == len(new_list):
+#                             result += [word]
+#                     else:
+#                         break
+
+#             result.sort()
+#             if result == []:
+#                 print(".")
+#             else:
+#                 print(*result, sep="\n")
+#                 print(".")
+#             letter_list, N = input().split()
+#         except:
+#             print(".")
+#             letter_list, N = input().split()
+#             if(N == "0"):
+#                 break
+
+# if __name__ == "__main__":
+#     main(sys.argv[1:])
